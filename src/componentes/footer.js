9ebcca4b490ae1f0b4cd7d00c4b789a23eb2a92c
Mateor13 <mateo.torres@epn.edu.ec>
2024-07-21 01:15:23 -0500

    Footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -4,14 +4,20 @@ import logo2 from './img/logo.jpeg';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer>
       <ul>
-        <img src={logo2} class='img2'></img>
+        <img src={logo2} className='img2' alt=''></img>
         <li><a href="#privacy">Privacy Policy</a></li>
-        <li><a href="#terms">Terms of Service</a></li>
-        <li><a href="#contact">Contact Us</a></li>
+        <li><h3>Av. Los Pinos y Juan Bereche</h3></li>
+        <li><h4>Telefono:</h4>
+        <p><a href="https://wa.me/+593982694885" rel="noreferrer" target='_blank'>0982694885</a></p>
+          <h4>Email: </h4>
+          <p><a href="mailto:richard.soria@epn.edu.ec" rel="noreferrer" target='_blank'>richard.soria@epn.edu.ec</a></p>
+          <h4>Horarios de atención: </h4>
+          <p>08h00 a 20h00</p>
+          </li>
       </ul>
-      <p>&copy; 2024 Your Store. All rights reserved.</p>
+      <p className='derechos'>&copy; 2024 EcuaGizer. Todos los derechos reservados.</p>
     </footer>
   );
 };
